afacefa61f295773ea1b0b047cc94f9191988cfd
Abhishek Thakur <50978468+Abhishekthakur99@users.noreply.github.com>
2020-09-14 11:20:56 +0530

    Update simpleLinearRegression.py

--- Linear_Regression/simpleLinearRegression.py
+++ Linear_Regression/simpleLinearRegression.py
@@ -1,7 +1,7 @@
 #importing libraries....
 import pandas as pd 
 import numpy as np 
-import matplotlib.pyplot as plt`
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
